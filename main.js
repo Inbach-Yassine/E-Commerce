2e196ab7a06e9496a4dad55b1aa895309029934a
YaSsyn_Inba <111376798+Inbach-Yassine@users.noreply.github.com>
2023-02-27 16:33:48 +0100

    Navbar media

---
+++ main.js
@@ -0,0 +1,7 @@
+var Menu = document.querySelector("#navbar");
+var MenuBar = document.querySelector("#MenuToggele");
+
+MenuBar.addEventListener("click", () => {
+  Menu.classList.toggle("open");
+  document.getElementById("navbar").style.background = "#DFEAF2";
+});
