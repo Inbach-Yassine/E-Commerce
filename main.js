0db31b5240e7d6dd8dc9c9524b71c296b7b220d1
YaSsyn_Inba <111376798+Inbach-Yassine@users.noreply.github.com>
2023-03-02 21:56:41 +0100

    modification for js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var Menu = document.querySelector("#navbar");
 var MenuBar = document.querySelector("#MenuToggle");
+var Header = document.querySelector("#header");
 
+window.addEventListener("scroll" , (e) =>{
+  if(e.target.id !== 'Menu' && e.target.id !== 'MenuBar' && e.target.id !== 'Header'){
+    Menu.classList.remove("open");
+}
+});
 MenuBar.addEventListener("click", () => {
   Menu.classList.toggle("open");
   document.getElementById("navbar").style.background = "#DFEAF2";
